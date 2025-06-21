a3bdd99a4e9c6feae9df38963f57f29e85d6768b
JuandaDevPragma <juan.ojeda@pragma.com.co>
2025-06-20 19:17:24 -0500

    fix js and ts compatibility

--- src/logger.ts
+++ src/logger.ts
@@ -1,23 +1,30 @@
 import * as winston from "winston";
+import  * as moment from "moment-timezone";
 import './tracing';
 import {getCurrentTrace} from "./tracing";
 
 export const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp(),
+        winston.format.timestamp({
+            format: () => moment.tz('America/Bogota').format('YYYY-MM-DDTHH:mm:ss.SSS')
+        }),
         winston.format.printf(({level, message, timestamp, ...obj} ) => {
             const { traceId, spanId } = getCurrentTrace();
 
+            const payload: any = obj.action;
+            const transaction_id = obj.transaction_id;
+
             const log = {
                 timestamp,
+                severity: level,
+                transaction_id: transaction_id ?? 'Id Not Available',
+                message,
                 tracing: {
                     traceId,
                     spanId
                 },
-                level,
-                message,
-                data: obj.action
+                action: payload
             };
 
             return JSON.stringify(log);
